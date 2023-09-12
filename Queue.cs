9cac57cea7c1c545eae3e7b158576f64d644b954
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-12 15:04:02 -0400

    Create c# queue constructor and properties

--- Queue.cs
+++ Queue.cs
@@ -1,7 +1,22 @@
 namespace Queues
 {
-    class Queue
+    class Queue<T>
     {
-        
+        public Node<T> Head
+        { get; set; }
+        public Node<T> Tail
+        { get; set; }
+        public int MaxSize
+        { get; }
+        public int Size
+        { get; private set; }
+
+        public Queue(int? maxSize = null)
+        {
+            Head = null;
+            Tail = null;
+            MaxSize = (int)maxSize;
+            Size = 0;
+        }
     }
 }
