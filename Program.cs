167dc4743bcd52502cfcbdb751a2cad4c360f082
BendyLand <128928536+BendyLand@users.noreply.github.com>
2023-09-12 15:31:38 -0400

    Add examples to program file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
+using System;
+
 namespace Queues
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Queue<string> q = new Queue<string>(4);
+            q.Enqueue("Ben");
+            q.Enqueue("Olivia");
+            q.Enqueue("Reese");
+            q.Enqueue("Puff");
 
+            Console.WriteLine($"Peek: {q.Peek()}");
+            Console.WriteLine($"Dequeue: {q.Dequeue()}");
+            Console.WriteLine($"Peek again: {q.Peek()}");
+            q.Enqueue("Too");
+            q.Enqueue("Many");
         }
     }
 }
